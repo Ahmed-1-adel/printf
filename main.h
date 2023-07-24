764570883f92d1ec2584bff63c7af92da74f9509
Ahmed-1-adel <Ahmedadel123321m@gmail.com>
2023-07-24 00:19:12 -0700

    printf project 100%

--- main.h
+++ main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -23,7 +24,7 @@
  *
  * @fmt: The format.
  * @fn: The function associated.
-*/
+ */
 struct fmt
 {
 	char fmt;
@@ -36,7 +37,7 @@ struct fmt
  *
  * @fmt: The format.
  * @fm_t: The function associated.
-*/
+ */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
